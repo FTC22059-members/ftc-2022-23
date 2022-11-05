d7904548ffbbdb6aa0d2cd180c018618f76c63b8
Alex <alexanderesolis@gmail.com>
2022-11-05 11:54:17 -0400

    Added a program for testing the arm motor

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/armTest.java
@@ -0,0 +1,48 @@
+/*
+2020-2021 FIRST Tech Challenge Team 14853
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp(name = "Test Arm")
+public class armTest extends LinearOpMode {
+    private DcMotor armMotor;
+
+    @Override
+    public void runOpMode() {
+        armMotor = hardwareMap.get(DcMotor.class, "arm");
+
+        // Wait for the start button
+        telemetry.addData(">", "Press Start to energize the robot with electrons that make it MOVE!");
+        telemetry.update();
+
+
+        waitForStart();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            armMotor.setPower(gamepad1.right_trigger);
+            telemetry.addData("Arm Power", gamepad1.right_trigger);
+            telemetry.update();
+
+            idle();
+        }
+    }
+}
