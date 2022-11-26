e1b7e84c17b0b54c1b715be304ef5251159a629f
Isaiah08-D <isaiahgrisha@gmail.com>
2022-11-26 12:09:42 -0500

    Add global positioning stuff

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/imu.java
@@ -0,0 +1,127 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+public class imu {
+    
+    private HardwareMap hardwareMap;
+    private Telemetry telemetry;
+    
+    private BNO055IMU imu;
+    private Orientation lastAngles = new Orientation();
+    private double globalAngle ;
+    
+    
+    
+    
+    public imu(HardwareMap hardwareMapCon, Telemetry telemetryCon) {
+        this.hardwareMap = hardwareMapCon;
+        this.telemetry = telemetryCon;
+    }
+    
+    public void resetAngle()
+    {
+        lastAngles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+        
+        globalAngle = 0;
+    }
+    
+    /**
+     * Get current cumulative angle rotation from last reset.
+     * @return Angle in degrees. + = left, - = right.
+     */
+    public double getAngle()
+    {
+        // We experimentally determined the Z axis is the axis we want to use for heading angle.
+        // We have to process the angle because the imu works in euler angles so the Z axis is
+        // returned as 0 to +180 or 0 to -180 rolling back to -179 or +179 when rotation passes
+        // 180 degrees. We detect this transition and track the total cumulative angle of rotation.
+        
+        Orientation angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+        
+        double deltaAngle = angles.firstAngle - lastAngles.firstAngle;
+        
+        if (deltaAngle < -180)
+            deltaAngle += 360;
+        else if (deltaAngle > 180)
+            deltaAngle -= 360;
+        
+        globalAngle += deltaAngle;
+        
+        lastAngles = angles;
+        
+        return globalAngle;
+    }
+    
+    public double getAngleRadians() {
+        // convert degrees to radians
+        return (Math.PI * this.getAngle()) / 180;
+    }
+    
+    public double checkDirection()
+    {
+        // The gain value determines how sensitive the correction is to direction changes.
+        // You will have to experiment with your robot to get small smooth direction changes
+        // to stay on a straight line.
+        double correction, angle, gain = .10;
+        
+        angle = getAngle();
+        
+        if (angle == 0)
+            correction = 0;             // no adjustment.
+        else
+            correction = -angle;        // reverse sign of angle for correction.
+            
+        correction = correction * gain;
+        
+        return correction;
+    }
+    
+    
+    
+    public void init() {
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+    
+        parameters.mode                = BNO055IMU.SensorMode.IMU;
+        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.loggingEnabled      = false;
+    
+        // Retrieve and initialize the IMU. We expect the IMU to be attached to an I2C port
+        // on a Core Device Interface Module, configured to be a sensor of type "AdaFruit IMU",
+        // and named "imu".
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+    
+        imu.initialize(parameters);
+    
+        telemetry.addData("Mode", "calibrating...");
+        telemetry.update();
+    }
+    
+    
+    public boolean isImuCalibrated() {
+        return imu.isGyroCalibrated();
+    }
+    
+    
+    public void preLoop() {
+    
+    }
+    
+    
+    public void imuLoop() {
+        telemetry.addData("1 imu heading", lastAngles.firstAngle);
+        telemetry.addData("2 global heading", globalAngle);
+        telemetry.update();
+    }
+    
+}
