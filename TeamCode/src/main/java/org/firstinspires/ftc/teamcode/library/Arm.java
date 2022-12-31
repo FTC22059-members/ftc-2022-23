3f2078b1b8dd9b00a93a5e06ef297ad72a586dd8
cleenex12 <monxide2@gmail.com>
2022-12-31 12:01:05 -0500

    arm movement controller bindings
    made arm movement the left stick rather than the right stick on gamepad2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/library/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/library/Arm.java
@@ -76,13 +76,13 @@ public class Arm {
      **/
 
     public void armLoop() {
-        telemetry.addData("Right stick Y:",gamepad2.right_stick_y);
-        if (gamepad2.right_stick_y < 0) {
+        telemetry.addData("Left stick Y:",gamepad2.left_stick_y);
+        if (gamepad2.left_stick_y < 0) {
             telemetry.addData("moving:","up");
-            moveUp(gamepad2.right_stick_y);
-        } else if (gamepad2.right_stick_y > 0) {
+            moveUp(gamepad2.left_stick_y);
+        } else if (gamepad2.left_stick_y > 0) {
             telemetry.addData("moving:","down");
-            moveDown(gamepad2.right_stick_y);
+            moveDown(gamepad2.left_stick_y);
         } else {
             armMotor.setPower(0);
         }
